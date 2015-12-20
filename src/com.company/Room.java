6ec6134d76d46a9db89fd5abd18828bf947e1f7f
WinPooh32 <winpooh32@yandex.ru>
2015-12-20 17:52:51 +1000

    minor refactoring

--- src/com.company/Room.java
+++ src/com.company/Room.java
@@ -9,10 +9,14 @@ public class Room {
 
     private Map _map;
 
+    static int _num = 0;
+
     public Room(){
         _firstWorkerTurn = true;
         _full = false;
         _map = new Map(3, 3, true);
+        _workerA = null;
+        _workerB = null;
     }
 
     public boolean Add(ClientWorker worker){
@@ -30,13 +34,14 @@ public class Room {
 
     public boolean MakeTurn(int player, int x, int y){
         if((_firstWorkerTurn && player == 1) || (!_firstWorkerTurn && player == 2)){
+            _firstWorkerTurn  = _firstWorkerTurn ? false : true;
             return _map.MakeTurn(player, x, y);
         }
 
         return  false;
     }
 
-    public boolean IsFirstWorkerTurn() { return _firstWorkerTurn; }
+    public boolean IsFirstPlayerTurn() { return _firstWorkerTurn; }
     public boolean IsFull(){
         return _full;
     }
